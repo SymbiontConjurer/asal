d5ae047f0bfbfbc1585bbe3ecb26995814b53c1f
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2025-01-01 20:36:02 -0800

    Update params

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 python main_opt.py \
-    --seed=0 \
+    --seed=1 \
     --save_dir="./data/supervised_0" \
     --substrate='lenia' \
     --time_sampling=1 \
-    --prompts="a happy smiley face" \
+    --prompts="a smiley face" \
     --coef_prompt=1. \
     --coef_softmax=0. \
     --coef_oe=0. \
     --bs=1 \
     --pop_size=32 \
-    --n_iters=1000 \
+    --rollout_steps=100 \
+    --n_iters=10000 \
     --sigma=0.1
