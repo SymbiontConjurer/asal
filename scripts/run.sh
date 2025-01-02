f8a93fccc7596dbe176bc4339f6bc938011c083f
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2025-01-01 20:11:34 -0800

    Add run.sh script

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+!#/usr/env/bash
+python main_opt.py \
+    --seed=0 \
+    --save_dir="./data/supervised_0" \
+    --substrate='lenia' \
+    --time_sampling=1 \
+    --prompts="a happy smiley face" \
+    --coef_prompt=1. \
+    --coef_softmax=0. \
+    --coef_oe=0. \
+    --bs=1 \
+    --pop_size=32 \
+    --n_iters=1000 \
+    --sigma=0.1
