9f06538381d5eb1d25a8b50658d497f130faae73
theaethermancer <135414057+SymbiontConjurer@users.noreply.github.com>
2025-01-02 23:00:39 +0000

    Just playing around with stuff

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 python main_opt.py \
-    --seed=1 \
+    --seed=2 \
     --save_dir="./data/supervised_0" \
     --substrate='lenia' \
     --time_sampling=1 \
-    --prompts="a smiley face" \
+    --prompts="a long caterpillar" \
     --coef_prompt=1. \
     --coef_softmax=0. \
     --coef_oe=0. \
     --bs=1 \
-    --pop_size=1024 \
+    --pop_size=32 \
     --rollout_steps=2048 \
-    --n_iters=10000 \
+    --n_iters=100 \
     --sigma=0.1
